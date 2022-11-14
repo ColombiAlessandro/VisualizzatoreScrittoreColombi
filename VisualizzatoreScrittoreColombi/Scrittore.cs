b6815fa291ed717bcd7ae83f1cda371893f5f364
ColombiAlessandro <94958005+ColombiAlessandro@users.noreply.github.com>
2022-11-14 10:22:49 +0100

    Aggiunta classi
    aggiunta delle classi del visualizzatore e scrittore

---
+++ VisualizzatoreScrittoreColombi/Scrittore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualizzatoreScrittoreColombi
+{
+    public class Scrittore
+    {
+        private Visualizzatore visualizzatore;
+        private string utente;
+        public Scrittore (string utente, Visualizzatore visualizzatore)
+        {
+            this.utente = utente;
+            this.visualizzatore = visualizzatore;
+        }
+        public void setGrassetto(bool g)
+        {
+            visualizzatore.Grassetto = g;
+        }
+        public void setSottolineato(bool s)
+        {
+            visualizzatore.Sottolineato = s;
+        }
+        public void setCorsivo(bool c)
+        {
+            visualizzatore.Corsivo = c;
+        }
+        public void setColore(Color c)
+        {
+            visualizzatore.Colore = c;
+        }
+        public void aggiungiTesto(string testo)
+        {
+            visualizzatore.Testo = testo;
+        }
+    }
+}
---
+++ VisualizzatoreScrittoreColombi/Visualizzatore.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualizzatoreScrittoreColombi
+{
+    public class Visualizzatore
+    {
+        private Color _colore;
+        private bool _sottolineato;
+        private bool _corsivo;
+        private bool _grassetto;
+        private RichTextBox textBox;
+        private string _testo;
+        public string Testo
+        {
+            set
+            {
+                _testo = value;
+            }
+            get
+            {
+                return _testo;
+            }
+        }
+        public Color Colore
+        {
+            set
+            {
+                _colore = value;
+            }
+            get
+            {
+                return _colore;
+            }
+        }
+        public bool Sottolineato
+        {
+            set
+            {
+                _sottolineato = value;
+            }
+            get
+            {
+                return _sottolineato;
+            }
+        }
+        public bool Corsivo
+        {
+            set
+            {
+                _corsivo = value;
+            }
+            get
+            {
+                return _corsivo;
+            }
+        }
+        public bool Grassetto
+        {
+            set
+            {
+                _grassetto = value;
+            }
+            get
+            {
+                return _grassetto;
+            }
+        }
+        public void Visualizza()
+        {
+            textBox.Text = _testo;
+            if (Grassetto)
+            {
+                textBox.SelectionFont = new Font(textBox.Font, FontStyle.Bold);
+            }
+            if (Sottolineato)
+            {
+                textBox.SelectionFont = new Font(textBox.Font, FontStyle.Underline);
+            }
+            if (Corsivo)
+            {
+                textBox.SelectionFont = new Font(textBox.Font, FontStyle.Italic);
+            }
+            if (Colore != null)
+            {
+                textBox.SelectionColor = Colore;
+            }
+        }
+    }
+}
